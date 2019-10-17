a90c857f8e80119b0a79142a3849f58fd6d1c0f2
Userbit <34487074+Userbit@users.noreply.github.com>
2019-10-17 18:43:39 +0300

    build&test: run(), end(), runStage:*() in process.js

--- handyGrabber/__test__/forData/process.test.js
+++ handyGrabber/__test__/forData/process.test.js
@@ -3,6 +3,8 @@ const proc = require('../../src/forData/process')
 const dbModelPath = '../../src/forData/dbModel';
 const dbModel = require(dbModelPath)
 
+const stages = ['first', 'upsert', 'check']
+
 afterEach(() => {
     jest.restoreAllMocks()
 })
@@ -24,10 +26,56 @@ describe('testing forData/process.js', () => {
 
         expect(dbModel.init).toBeCalledWith(cliMock.argv.entity)
         expect(dbMock.createIndex).toBeCalledTimes(1)
-        expect(proc.run).toBeCalledWith(cliMock, dbMock)
+        expect(proc.run).toBeCalledWith(cliMock)
     })
 
-    test('run() method should ...', async () => {
-        expect(true).toBe(true)
+    test('end() should call this.db.close', async () => {
+        const dbReal = proc.db
+        proc.db = { close: jest.fn().mockResolvedValue({}) }
+
+        await proc.end()
+
+        expect(proc.db.close).toBeCalledTimes(1)
+
+        proc.db = dbReal
+    })
+
+    test('run() should call runStage:first|upsert|check method one time when entity==movie', async () => {
+        for (let stage of stages) {
+            const runStage = 'runStage:' + stage
+            const state = { stage, entity: 'movie', log: 1 }
+            const cliMock = { argv: { ...state, trash: 'trash' } }
+            jest.spyOn(proc, runStage).mockResolvedValue({})
+            jest.spyOn(proc, 'end').mockResolvedValue({})
+
+            await proc.run(cliMock)
+
+            expect(proc[runStage])
+                .toBeCalledTimes(1)
+                .toBeCalledWith(state)
+            expect(proc.end).toBeCalledTimes(1)
+
+            jest.restoreAllMocks()
+        }
+    })
+
+    test('run() should call runStage:first|upsert|check method Two times when entity==torrent', async () => {
+        for (let stage of stages) {
+            const runStage = 'runStage:' + stage
+            const state = { stage, entity: 'torrent', log: 0 }
+            const cliMock = { argv: { ...state, trash: 'trash' } }
+            jest.spyOn(proc, runStage).mockResolvedValue({})
+            jest.spyOn(proc, 'end').mockResolvedValue({})
+
+            await proc.run(cliMock)
+
+            expect(proc[runStage])
+                .toBeCalledTimes(2)
+                .nthCalledWith(1, { ...state, deleted: true })
+                .nthCalledWith(2, { ...state, deleted: false })
+            expect(proc.end).toBeCalledTimes(1)
+
+            jest.restoreAllMocks()
+        }
     })
 })
--- handyGrabber/src/forData/process.js
+++ handyGrabber/src/forData/process.js
@@ -1,4 +1,4 @@
-const { debug, sleep } = require('../util')(module)
+const { debug, sleep, getObjectForKeys } = require('../util')(module)
 const dbModel = require('./dbModel')
 
 module.exports = {
@@ -6,20 +6,53 @@ module.exports = {
     async init(cli) {
         debug('Start forData/process.js')
 
-        const db = await dbModel.init(cli.argv.entity)
-        await db.createIndex()
+        this.db = await dbModel.init(cli.argv.entity)
+        await this.db.createIndex()
 
         sleep(10)
-        await this.run(cli, db)
+        await this.run(cli)
 
     },
 
-    async run(cli, db) {
+    async run(cli) {
         debug('Running...')
 
+        // Get cleaned state from cli.argv out of specified keys for the sake of clarity
+        let state = getObjectForKeys(['stage', 'entity', 'log'], cli.argv)
+        const runStage = 'runStage:' + state.stage
+
+        switch(state.entity) {
+            case 'movie':
+                await this[runStage](state)
+                break
+
+            case 'torrent':
+                state = { ...state, deleted: true }
+                await this[runStage](state)
+
+                state = { ...state, deleted: false }
+                await this[runStage](state)
+
+                break
+        }
+
+        await this.end()
     },
 
-    async end(cli, db) {
-        db.close()
+    async end() {
+        debug('Ending...')
+        await this.db.close()
     },
+
+    'runStage:first': async function(state) {
+        debug('runStage:first(%o)...', state)
+    }, 
+
+    'runStage:upsert': async function(state) {
+        debug('runStage:upsert(%o)...', state)
+    }, 
+
+    'runStage:check': async function(state) {
+        debug('runStage:check(%o)...', state)
+    }, 
 }
