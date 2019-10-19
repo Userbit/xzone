aefafe64720162608c1ec033f609f7d6657530fb
Userbit <34487074+Userbit@users.noreply.github.com>
2019-10-19 16:51:54 +0300

    built&test: init() & related funcs in request.js

--- handyGrabber/__test__/forData/request.test.js
+++ handyGrabber/__test__/forData/request.test.js
@@ -1,7 +1,65 @@
-const request = require('../../src/forData/request')
+const request = require('request')
+const req = require('../../src/forData/request')
+
+const entities = ['movie', 'torrent']
+
+afterEach(() => {
+    jest.restoreAllMocks()
+    req.baseRequest = {}
+})
 
 describe('testing forData/request.js', () => {
-    it('jrequest.js should be required', () => {
-        expect(request).toBeTruthy()
+    it('request.js should be required', () => {
+        expect(req).toBeTruthy()
     })
+
+    test('init() should call request.defaults(opts), getOptsFor:movie|torrent()', async () => {
+        for(const entity of entities) {
+            const getOptsFor = 'getOptsFor:' + entity
+            const mockObj = { mockProp: 'mockVal' }
+            const state = { entity, stage: 'some stage', log: 1, deleted: true }
+            jest.spyOn(request, 'defaults').mockReturnValueOnce(mockObj)
+            jest.spyOn(req, getOptsFor).mockImplementationOnce(() => {})
+
+            req.init(state)
+
+            expect(request.defaults).toBeCalledWith(expect.toContainAllKeys([
+                'baseUrl',
+                'json',
+                'method',
+                'headers',
+                'gzip',
+                'uri',
+                'qs',
+            ]))
+            expect(req[getOptsFor]).toBeCalledWith(state)
+            expect(req.baseRequest).toStrictEqual(mockObj)
+
+            jest.restoreAllMocks()
+        }
+    })
+
+    test('getUri(entity) should return correct uri string for entity', () => {
+        const entity = 'movie'
+        const expectedUri = '/movie/select/'
+
+        expect(req.getUri(entity)).toBe(expectedUri)
+    })
+
+    test('getOptsFor:movie() should correct opts', async () => {
+        const state = { entity: 'movie', stage: 'some stage', log: 1 }
+        const expectedOpts = { uri: '/movie/select/' }
+
+        expect(req["getOptsFor:movie"](state)).toStrictEqual(expectedOpts)
+    })
+
+    test('getOptsFor:torrent() should call baseRequest.defaults(opts) correctly', async () => {
+        for(const deleted of [true, false]) {
+            const state = { entity: 'torrent', deleted, stage: 'some stage', log: 1 }
+            const expectedOpts = { uri: '/torrent/select/', qs: { fq: 'deleted:' + deleted } }
+            
+            expect(req["getOptsFor:torrent"](state)).toStrictEqual(expectedOpts)
+        }
+    })
+
 })
--- handyGrabber/src/forData/request.js
+++ handyGrabber/src/forData/request.js
@@ -1,11 +1,12 @@
 const request = require('request')
-const { debug } = require('../util')
+const { debug, sleep, ...util } = require('../util')
 
 
 const defaultOptions = {
     baseUrl: 'some_valid_url',
     json: true,
     method: 'GET',
+    gzip: true,
     headers: {
         'User-Agent': 'Java/1.8.0_212',
         'Accept-Encoding': 'gzip, deflate',
@@ -13,13 +14,55 @@ const defaultOptions = {
         'Accept': 'text/html, image/gif, image/jpeg, *; q=.2, */*; q=.2',
         'Connection': 'keep-alive',
     },
-    gzip: true,
-};
+}
 
-const baseRequest = request.defaults(defaultOptions)
+const queryOptions = {
+    uri: '/{movie|torrent}/select/',
+    qs: {
+        q: '(*:*)',
+        sort: '',
+        // sort: 'id asc',
+        // sort: 'add_date desc',
+        fl: '*',
+        fq: '',
+        start: 0,
+        rows: 0,
+        version: '2.2',
+        wt: 'json1',
+    }
+}
+
 
 module.exports = {
-    init(state) {
 
+    baseRequest: {},
+
+    init(state) {
+        const entityOptions = this['getOptsFor:' + state.entity](state)
+        this.baseRequest = request.defaults({ 
+            ...defaultOptions, 
+            ...queryOptions,
+            ...entityOptions,
+        })
     },
+
+    'getOptsFor:movie': function(state) {
+        return { 
+            uri: this.getUri(state.entity),
+        }
+    },
+
+    'getOptsFor:torrent': function(state) {
+        return {
+            uri: this.getUri(state.entity),
+            qs: {
+                fq: 'deleted:' + state.deleted,
+            },
+        }
+    },
+
+    getUri(entity) {
+        return `/${entity}/select/`
+    }
+
 }
