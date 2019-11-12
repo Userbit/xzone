cdce7802d07049ffe23676e4d93e145fbb1a96a0
Userbit <34487074+Userbit@users.noreply.github.com>
2019-11-12 20:30:57 +0300

    ESLint for app.js

--- handyGrabber/src/app.js
+++ handyGrabber/src/app.js
@@ -1,30 +1,36 @@
+const cli = require('./cli');
 
 const initCli = () => {
-    const cli = require('./cli')
-    cli.argv = cli.argv
-        // provide a minimum demand and a minimum demand message
-        .demandCommand(1, 'You need at least one command before moving on')
-        .help()
-        .argv
+  cli.argv = cli.argv
+    // provide a minimum demand and a minimum demand message
+    .demandCommand(1, 'You need at least one command before moving on')
+    .help()
+    .argv;
 
-    return cli
+  return cli;
+};
+
+// eslint-disable-next-line consistent-return
+const start = async (initedCli) => {
+  if (initedCli.isData(initedCli.argv._[0])) {
+    // Start processing of data
+    // eslint-disable-next-line global-require
+    return require('./forData/process').init(initedCli);
+  }
+
+  if (initedCli.isImg(initedCli.argv._[0])) {
+    // Start processing of images
+    // eslint-disable-next-line global-require
+    return require('./forImg/process').init(initedCli);
+  }
+};
+
+module.exports = {
+  start,
+  initCli,
+};
+
+
+if (!module.parent) {
+  start(initCli());
 }
-
-const start = async (cli) => {
-    if (cli.isData(cli.argv._[0])) {
-        // Start processing of data
-        return await require('./forData/process').init(cli)
-    } else if (cli.isImg(cli.argv._[0])) {
-        // Start processing of images
-        return await require('./forImg/process').init(cli)
-    }
-}
-
-module.exports = { 
-    start, 
-    initCli,
-}
-
-
-if (!module.parent)
-    start(initCli())
