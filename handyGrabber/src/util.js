e50066d31691c325080c5d2b4f0dcb8a4c4b29a2
Userbit <34487074+Userbit@users.noreply.github.com>
2019-11-12 20:52:06 +0300

    eslint util.js

--- handyGrabber/src/util.js
+++ handyGrabber/src/util.js
@@ -1,44 +1,49 @@
+const childProcess = require('child_process');
+const debugModule = require('debug');
 
-const rootdir = 'xzone'
+const rootdir = 'xzone';
+
+function getNamespace(externalModule, rootDir) {
+  const { filename } = externalModule;
+  const namespace = filename.slice(filename.indexOf(rootDir));
+  return `${namespace}:`;
+}
 
 function getDebug(externalModule) {
-    return require('debug')(getNamespace(externalModule, rootdir))
-} 
-
-function getNamespace(externalModule, rootdir) {
-    const filename = externalModule.filename
-    const namespace = filename.slice(filename.indexOf(rootdir))
-    return namespace + ':'
+  return debugModule(getNamespace(externalModule, rootdir));
 }
 
 function sleep(externalModule) {
-    let debug = getDebug(externalModule)
-    return (delayInSec) => {
-        const child_process = require("child_process");
-        debug && debug(`Sleep on ${delayInSec} sec...`)
-        child_process.execSync("sleep " + delayInSec)
-    }
+  const debug = getDebug(externalModule);
+  return (delayInSec) => {
+    // eslint-disable-next-line no-unused-expressions
+    debug && debug(`Sleep on ${delayInSec} sec...`);
+    childProcess.execSync(`sleep ${delayInSec}`);
+  };
 }
 
 function getObjectForKeys(keys, fromObject) {
-    return keys.reduce((obj, key) => {
-        if (fromObject[key])
-            obj[key] = fromObject[key]
-        return obj
-    }, {})
+  return keys.reduce((obj, key) => {
+    if (fromObject[key]) {
+      // eslint-disable-next-line no-param-reassign
+      obj[key] = fromObject[key];
+    }
+
+    return obj;
+  }, {});
 }
 
 
 module.exports = (externalModule) => {
-    if (!(externalModule instanceof module.constructor)) {
-        const errMsg = "require('path/to/util')(module) should be passed Module object of caller"
-        throw Error(errMsg)
-    }
+  if (!(externalModule instanceof module.constructor)) {
+    const errMsg = "require('path/to/util')(module) should be passed Module object of caller";
+    throw Error(errMsg);
+  }
 
-    return {
-        debug: getDebug(externalModule),
-        sleep: sleep(externalModule),
-        getNamespace,
-        getObjectForKeys,
-    }
-}
+  return {
+    debug: getDebug(externalModule),
+    sleep: sleep(externalModule),
+    getNamespace,
+    getObjectForKeys,
+  };
+};
