3516a6b3a270f4def890a55d7f7cf96379bcd197
Userbit <34487074+Userbit@users.noreply.github.com>
2019-11-13 17:02:03 +0300

    eslint forData/dbModel.js

--- handyGrabber/src/forData/dbModel.js
+++ handyGrabber/src/forData/dbModel.js
@@ -1,65 +1,69 @@
 
+const { debug } = require('../util')(module);
+const { DbConn } = require('../db');
 
-const { debug, sleep } = require('../util')(module)
-const { DbConn } = require('../db')
-
-let objDb = null
+let objDb = null;
 
 module.exports = {
-    async init(entity) {
-        this.checkEntity(entity)
-        this.entity = entity
+  async init(entity) {
+    this.checkEntity(entity);
+    this.entity = entity;
 
-        if (objDb) return objDb
-        objDb = this
+    if (objDb) return objDb;
+    objDb = this;
 
-        this.dbConn = new DbConn()
-        this.db = await this.dbConn.getDb()
-        this.docs = this.db.collection(entity)
-        this.log = this.db.collection('log')
-        debug("Connected successfully to MongoDb.");
+    this.dbConn = new DbConn();
+    this.db = await this.dbConn.getDb();
+    this.docs = this.db.collection(entity);
+    this.log = this.db.collection('log');
+    debug('Connected successfully to MongoDb.');
 
-        return this
-    },
+    return this;
+  },
 
-    checkEntity(entity) {
-        if (!['movie', 'torrent'].includes(entity))
-            throw Error('"entity" argurment should be `movie` or `torrent`.')
-    },
+  checkEntity(entity) {
+    if (!['movie', 'torrent'].includes(entity)) {
+      throw Error('"entity" argurment should be `movie` or `torrent`.');
+    }
+  },
 
-    async close() {
-        await this.dbConn.close();
-        objDb = null;
-        debug("Connection to MongoDb successfully closed.");
-    },
+  async close() {
+    await this.dbConn.close();
+    objDb = null;
+    debug('Connection to MongoDb successfully closed.');
+  },
 
-    async createIndex() {
-        let indexSpecs, options = { background: true, }
+  async createIndex() {
+    let indexSpecs; const
+      options = { background: true };
 
-        switch (this.entity) {
-            case 'torrent':
-                indexSpecs = [
-                    { 'deleted': -1 },
-                    { "_id": 1 },
-                ]
-                break
-            case 'movie':
-                indexSpecs = [
-                    { "_id": 1 }, 
-                ]
-                break
-        }
+    switch (this.entity) {
+      case 'torrent':
+        indexSpecs = [
+          { deleted: -1 },
+          { _id: 1 },
+        ];
+        break;
 
-        // if (!indexSpecs) return null
+      case 'movie':
+        indexSpecs = [
+          { _id: 1 },
+        ];
+        break;
 
-        try {
-            for (let indexSpec of indexSpecs) {
-                const opts = indexSpec._id ? {} : options;
-                const res = await this.docs.createIndex(indexSpec, opts)
-                debug('Result of creating index:', res)
-            }
-        } catch (err) {
-            throw err
-        }
-    },
-}
+      default:
+        break;
+    }
+
+    // if (!indexSpecs) return null
+
+    // eslint-disable-next-line no-restricted-syntax
+    for (const indexSpec of indexSpecs) {
+      // eslint-disable-next-line no-underscore-dangle
+      const opts = indexSpec._id ? {} : options;
+      // eslint-disable-next-line no-await-in-loop
+      const res = await this.docs.createIndex(indexSpec, opts);
+      debug('Result of creating index:', res);
+    }
+  },
+};
