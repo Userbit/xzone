419a8b3d33df22f7051b44be9eb2164add2d75f6
Userbit <34487074+Userbit@users.noreply.github.com>
2019-11-13 20:14:17 +0300

    refine forData/process.js

--- handyGrabber/src/forData/process.js
+++ handyGrabber/src/forData/process.js
@@ -1,74 +1,72 @@
-const { debug, sleep, ...util } = require('../util')(module)
-const dbModel = require('./dbModel')
-const log = require('./log')
-const req = require('./request')
+const { debug, sleep, ...util } = require("../util")(module);
+const dbModel = require("./dbModel");
+const log = require("./log");
+const req = require("./request");
 
 module.exports = {
+  async init(cli) {
+    debug("Start forData/process.js");
 
-    async init(cli) {
-        debug('Start forData/process.js')
+    this.db = await dbModel.init(cli.argv.entity);
+    await this.db.createIndex();
 
-        this.db = await dbModel.init(cli.argv.entity)
-        await this.db.createIndex()
+    sleep(10);
+    await this.run(cli);
+  },
 
-        sleep(10)
-        await this.run(cli)
+  async run(cli) {
+    debug("Running...");
 
-    },
+    // Get cleaned state from cli.argv out of specified keys for the sake of clarity
+    const state = util.getObjectForKeys(["stage", "entity", "log"], cli.argv);
 
-    async run(cli) {
-        debug('Running...')
+    await this[`runEntity:${state.entity}`](state);
+    await this.end();
+  },
 
-        // Get cleaned state from cli.argv out of specified keys for the sake of clarity
-        let state = util.getObjectForKeys(['stage', 'entity', 'log'], cli.argv)
+  async end() {
+    debug("Ending...");
+    await this.db.close();
+  },
 
-        await this['runEntity:' + state.entity](state)
-        await this.end()
-    },
+  "runEntity:movie": async function movie(state) {
+    debug("runEntity:movie(%o)...", state);
+    await this.runStage(state);
+  },
 
-    async end() {
-        debug('Ending...')
-        await this.db.close()
-    },
+  "runEntity:torrent": async function torrent(state) {
+    debug("runEntity:torrent(%o)...", state);
 
-    'runEntity:movie': async function(state) {
-        debug('runEntity:movie(%o)...', state)
-        await this.runStage(state)
-    }, 
+    let newState = { ...state, deleted: true };
+    await this.runStage(newState);
 
-    'runEntity:torrent': async function(state) {
-        debug('runEntity:torrent(%o)...', state)
+    newState = { ...newState, deleted: false };
+    await this.runStage(newState);
+  },
 
-        state = { ...state, deleted: true }
-        await this.runStage(state)
+  async runStage(state) {
+    debug("runStage(%o)...", state);
 
-        state = { ...state, deleted: false }
-        await this.runStage(state)
-    }, 
+    await this.initSubModules(state);
+    await this[`runStage:${state.stage}`](state);
+  },
 
-    async runStage(state) {
-        debug('runStage(%o)...', state)
+  async initSubModules(state) {
+    debug("initSubModules(%o)...", state);
 
-        await this.initSubModules(state)
-        await this['runStage:' + state.stage](state)
-    }, 
+    log.init(state);
+    req.init(state);
+  },
 
-    async initSubModules(state) {
-        debug('initSubModules(%o)...', state)
+  "runStage:first": async function first(state) {
+    debug("runStage:first(%o)...", state);
+  },
 
-        log.init(state)
-        req.init(state)
-    },
+  "runStage:upsert": async function upsert(state) {
+    debug("runStage:upsert(%o)...", state);
+  },
 
-    'runStage:first': async function(state) {
-        debug('runStage:first(%o)...', state)
-    }, 
-
-    'runStage:upsert': async function(state) {
-        debug('runStage:upsert(%o)...', state)
-    }, 
-
-    'runStage:check': async function(state) {
-        debug('runStage:check(%o)...', state)
-    }, 
-}
+  "runStage:check": async function check(state) {
+    debug("runStage:check(%o)...", state);
+  },
+};
