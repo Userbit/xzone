624eaea5d9d8f6173b2eda64467913250795d425
Userbit <34487074+Userbit@users.noreply.github.com>
2019-11-13 19:30:35 +0300

    prettier forData/dbModel.js

--- handyGrabber/src/forData/dbModel.js
+++ handyGrabber/src/forData/dbModel.js
@@ -1,6 +1,5 @@
-
-const { debug } = require('../util')(module);
-const { DbConn } = require('../db');
+const { debug } = require("../util")(module);
+const { DbConn } = require("../db");
 
 let objDb = null;
 
@@ -15,14 +14,14 @@ module.exports = {
     this.dbConn = new DbConn();
     this.db = await this.dbConn.getDb();
     this.docs = this.db.collection(entity);
-    this.log = this.db.collection('log');
-    debug('Connected successfully to MongoDb.');
+    this.log = this.db.collection("log");
+    debug("Connected successfully to MongoDb.");
 
     return this;
   },
 
   checkEntity(entity) {
-    if (!['movie', 'torrent'].includes(entity)) {
+    if (!["movie", "torrent"].includes(entity)) {
       throw Error('"entity" argurment should be `movie` or `torrent`.');
     }
   },
@@ -30,25 +29,20 @@ module.exports = {
   async close() {
     await this.dbConn.close();
     objDb = null;
-    debug('Connection to MongoDb successfully closed.');
+    debug("Connection to MongoDb successfully closed.");
   },
 
   async createIndex() {
-    let indexSpecs; const
-      options = { background: true };
+    let indexSpecs;
+    const options = { background: true };
 
     switch (this.entity) {
-      case 'torrent':
-        indexSpecs = [
-          { deleted: -1 },
-          { _id: 1 },
-        ];
+      case "torrent":
+        indexSpecs = [{ deleted: -1 }, { _id: 1 }];
         break;
 
-      case 'movie':
-        indexSpecs = [
-          { _id: 1 },
-        ];
+      case "movie":
+        indexSpecs = [{ _id: 1 }];
         break;
 
       default:
@@ -63,7 +57,7 @@ module.exports = {
       const opts = indexSpec._id ? {} : options;
       // eslint-disable-next-line no-await-in-loop
       const res = await this.docs.createIndex(indexSpec, opts);
-      debug('Result of creating index:', res);
+      debug("Result of creating index:", res);
     }
   },
 };
