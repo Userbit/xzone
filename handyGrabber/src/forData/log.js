0482d6263fa1dba6a0c170a67eac05fc5c65c3d3
Userbit <34487074+Userbit@users.noreply.github.com>
2019-11-13 19:46:49 +0300

    refine forData/log.js

--- handyGrabber/src/forData/log.js
+++ handyGrabber/src/forData/log.js
@@ -1,36 +1,38 @@
-const { debug, sleep, ...util } = require('../util')(module)
+const { debug, sleep, ...util } = require("../util")(module);
 
 module.exports = {
-    init(state) {
-        this.state = {
-            ...util.getObjectForKeys(['stage', 'entity', 'deleted'], state),
-            logStart: new Date(),
-        }
-        this.switchFuncsOnce(state)
+  init(state) {
+    this.state = {
+      ...util.getObjectForKeys(["stage", "entity", "deleted"], state),
+      logStart: new Date(),
+    };
+    this.switchFuncsOnce(state);
+  },
+
+  switchFuncsOnce(state) {
+    Object.entries(this.switchedFuncs).forEach(([funcName, func]) => {
+      if (this[funcName]) throw Error(`'${funcName}' property already exist on main object.`);
+
+      this[funcName] = state.log ? func : this.emptyAsyncFunc;
+    });
+
+    this.switchFuncsOnce = this.emptyFunc;
+  },
+
+  emptyFunc() {
+    // do nothing.
+  },
+  async emptyAsyncFunc() {
+    // do nothing.
+  },
+
+  switchedFuncs: {
+    async updateState() {
+      // TODO:
     },
 
-    switchFuncsOnce(state) {
-        for(const [funcName, func] of Object.entries(this.switchedFuncs)) {
-            if (this[funcName]) 
-                throw Error(`'${funcName}' property already exist on main object.`)
-
-            this[funcName] = state.log ? func : this.emptyAsyncFunc
-        }
-        
-        this.switchFuncsOnce = this.emptyFunc
+    async done() {
+      // TODO:
     },
-
-    emptyFunc() { },
-    async emptyAsyncFunc() { },
-
-    switchedFuncs: {
-        async updateState() {
-            // TODO:
-        },
-
-        async done() {
-            // TODO:
-        },
-    },
-
-}
+  },
+};
