4ded23c029bf7e6ca47ce063eb2c02522b894809
Userbit <34487074+Userbit@users.noreply.github.com>
2019-11-14 15:00:15 +0300

    refine forData/request.js

--- handyGrabber/src/forData/request.js
+++ handyGrabber/src/forData/request.js
@@ -1,67 +1,62 @@
-const request = require('request')
-const { debug, sleep, ...util } = require('../util')
-
+const request = require("request");
 
 const defaultOptions = {
-    baseUrl: 'some_valid_url',
-    json: true,
-    method: 'GET',
-    gzip: true,
-    headers: {
-        'User-Agent': 'Java/1.8.0_212',
-        'Accept-Encoding': 'gzip, deflate',
-        'Host': 'some_valid_url',
-        'Accept': 'text/html, image/gif, image/jpeg, *; q=.2, */*; q=.2',
-        'Connection': 'keep-alive',
-    },
-}
+  baseUrl: "some_valid_url",
+  json: true,
+  method: "GET",
+  gzip: true,
+  headers: {
+    "User-Agent": "Java/1.8.0_212",
+    "Accept-Encoding": "gzip, deflate",
+    Host: "some_valid_url",
+    Accept: "text/html, image/gif, image/jpeg, *; q=.2, */*; q=.2",
+    Connection: "keep-alive",
+  },
+};
 
 const queryOptions = {
-    uri: '/{movie|torrent}/select/',
-    qs: {
-        q: '(*:*)',
-        sort: '',
-        // sort: 'id asc',
-        // sort: 'add_date desc',
-        fl: '*',
-        fq: '',
-        start: 0,
-        rows: 0,
-        version: '2.2',
-        wt: 'json1',
-    }
-}
-
+  uri: "/{movie|torrent}/select/",
+  qs: {
+    q: "(*:*)",
+    sort: "",
+    // sort: 'id asc',
+    // sort: 'add_date desc',
+    fl: "*",
+    fq: "",
+    start: 0,
+    rows: 0,
+    version: "2.2",
+    wt: "json1",
+  },
+};
 
 module.exports = {
+  baseRequest: {},
 
-    baseRequest: {},
+  init(state) {
+    const entityOptions = this[`getOptsFor:${state.entity}`](state);
+    this.baseRequest = request
+      .defaults(defaultOptions)
+      .defaults(queryOptions)
+      .defaults(entityOptions);
+  },
 
-    init(state) {
-        const entityOptions = this['getOptsFor:' + state.entity](state)
-        this.baseRequest = request
-            .defaults(defaultOptions)
-            .defaults(queryOptions)
-            .defaults(entityOptions)
-    },
+  "getOptsFor:movie": function movie(state) {
+    return {
+      uri: this.getUri(state.entity),
+    };
+  },
 
-    'getOptsFor:movie': function(state) {
-        return { 
-            uri: this.getUri(state.entity),
-        }
-    },
+  "getOptsFor:torrent": function torrent(state) {
+    return {
+      uri: this.getUri(state.entity),
+      qs: {
+        fq: `deleted:${state.deleted}`,
+      },
+    };
+  },
 
-    'getOptsFor:torrent': function(state) {
-        return {
-            uri: this.getUri(state.entity),
-            qs: {
-                fq: 'deleted:' + state.deleted,
-            },
-        }
-    },
-
-    getUri(entity) {
-        return `/${entity}/select/`
-    }
-
-}
+  getUri(entity) {
+    return `/${entity}/select/`;
+  },
+};
