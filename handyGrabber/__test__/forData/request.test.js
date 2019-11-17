2168a80d0a8d2d757fbc1cc849c9894a51c37b65
Userbit <34487074+Userbit@users.noreply.github.com>
2019-11-17 15:00:36 +0300

    refine forData/request.test.js

--- handyGrabber/__test__/forData/request.test.js
+++ handyGrabber/__test__/forData/request.test.js
@@ -1,77 +1,63 @@
-const request = require('request')
-const req = require('../../src/forData/request')
+const request = require("request");
+const req = require("../../src/forData/request");
 
-const entities = ['movie', 'torrent']
+const entities = ["movie", "torrent"];
 
-afterEach(() => {
-    jest.restoreAllMocks()
-    req.baseRequest = {}
-})
+describe("testing forData/request.js", () => {
+  it("request.js should be required", () => {
+    expect(req).toBeObject();
+  });
 
-describe('testing forData/request.js', () => {
-    it('request.js should be required', () => {
-        expect(req).toBeTruthy()
-    })
+  it.each(entities)("init() should call request.defaults(opts), getOptsFor:%s()", (entity) => {
+    const getOptsFor = `getOptsFor:${entity}`;
+    const mockObj = { mockProp: "mockVal" };
+    const state = { entity, stage: "some stage", log: 1, deleted: true };
+    jest
+      .spyOn(request, "defaults")
+      .mockReturnValueOnce(request)
+      .mockReturnValueOnce(request)
+      .mockReturnValueOnce(mockObj);
+    jest.spyOn(req, getOptsFor).mockReturnValueOnce(mockObj);
 
-    test('init() should call request.defaults(opts), getOptsFor:movie|torrent()', async () => {
-        for(const entity of entities) {
-            const getOptsFor = 'getOptsFor:' + entity
-            const mockObj = { mockProp: 'mockVal' }
-            const state = { entity, stage: 'some stage', log: 1, deleted: true }
-            jest.spyOn(request, 'defaults')
-                .mockReturnValueOnce(request)
-                .mockReturnValueOnce(request)
-                .mockReturnValueOnce(mockObj)
-            jest.spyOn(req, getOptsFor).mockReturnValueOnce(mockObj)
+    req.init(state);
 
-            req.init(state)
+    expect(request.defaults)
+      .toHaveBeenNthCalledWith(
+        1,
+        expect.toContainAllKeys(["baseUrl", "json", "method", "headers", "gzip"])
+      )
+      .nthCalledWith(2, expect.toContainAllKeys(["uri", "qs"]))
+      .nthCalledWith(3, mockObj);
+    expect(req[getOptsFor]).toHaveBeenCalledWith(state);
+    expect(req.baseRequest).toStrictEqual(mockObj);
+  });
 
-            expect(request.defaults)
-                .nthCalledWith(1, expect.toContainAllKeys([
-                    'baseUrl',
-                    'json',
-                    'method',
-                    'headers',
-                    'gzip',
-                ]))
-                .nthCalledWith(2, expect.toContainAllKeys([
-                    'uri',
-                    'qs',
-                ]))
-                .nthCalledWith(3, mockObj)
-            expect(req[getOptsFor]).toBeCalledWith(state)
-            expect(req.baseRequest).toStrictEqual(mockObj)
+  it("getUri(entity) should return correct uri string for entity", () => {
+    const entity = "movie";
+    const expectedUri = "/movie/select/";
 
-            jest.restoreAllMocks()
-        }
-    })
+    expect(req.getUri(entity)).toBe(expectedUri);
+  });
 
-    test('getUri(entity) should return correct uri string for entity', () => {
-        const entity = 'movie'
-        const expectedUri = '/movie/select/'
+  it("getOptsFor:movie() should correct opts", async () => {
+    const state = { entity: "movie", stage: "some stage", log: 1 };
+    const expectedOpts = { uri: "/movie/select/" };
 
-        expect(req.getUri(entity)).toBe(expectedUri)
-    })
+    expect(req["getOptsFor:movie"](state)).toStrictEqual(expectedOpts);
+  });
 
-    test('getOptsFor:movie() should correct opts', async () => {
-        const state = { entity: 'movie', stage: 'some stage', log: 1 }
-        const expectedOpts = { uri: '/movie/select/' }
+  it.each([true, false])(
+    "getOptsFor:torrent({deleted: %s}) should call baseRequest.defaults(opts) correctly",
+    (deleted) => {
+      const state = { entity: "torrent", deleted, stage: "some stage", log: 1 };
+      const expectedOpts = {
+        uri: "/torrent/select/",
+        qs: {
+          fq: `deleted:${deleted}`,
+        },
+      };
 
-        expect(req["getOptsFor:movie"](state)).toStrictEqual(expectedOpts)
-    })
-
-    test('getOptsFor:torrent() should call baseRequest.defaults(opts) correctly', async () => {
-        for(const deleted of [true, false]) {
-            const state = { entity: 'torrent', deleted, stage: 'some stage', log: 1 }
-            const expectedOpts = {
-                uri: '/torrent/select/',
-                qs: { 
-                    fq: 'deleted:' + deleted
-                }
-            }
-            
-            expect(req["getOptsFor:torrent"](state)).toStrictEqual(expectedOpts)
-        }
-    })
-
-})
+      expect(req["getOptsFor:torrent"](state)).toStrictEqual(expectedOpts);
+    }
+  );
+});
